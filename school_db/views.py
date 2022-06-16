63cb486fef835e12223dc8c28d471e4e59fa17bc
Karim <aarrale12@gmail.com>
2022-06-16 17:28:40 -0500

    Query lab almost done

--- school_db/views.py
+++ school_db/views.py
@@ -284,9 +284,13 @@ VALUES ('Kyle', 'Harwood', 2022, 3.0)
 def problem_six(request):
     
     # Make sure to set this equal to the primary key of the row you just created!
-    Student.objects.filter(pk=student_id).update(gpa=3.5)
-    student = Student.objects.get(pk=student_id).update(year=2019)
     student_id = 11
+    Student.objects.filter(pk=student_id).update(gpa=3.5)
+    student = Student.objects.get(pk=student_id)
+    print(f'Id: {student.id}')
+    print(f'Full Name: {student.first_name} {student.last_name}')
+    print(f'GPA: {student.gpa}')
+    
     
 
 
